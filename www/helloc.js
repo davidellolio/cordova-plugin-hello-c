90e036fed1872c48080e29d7b8a29a3e88d43d15
davidellolio <davidellolio@gmail.com>
2019-09-12 01:21:05 +0200

    modifiche per plugin

--- www/helloc.js
+++ www/helloc.js
@@ -2,12 +2,6 @@
 
 module.exports = {
     getArch: function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "HelloCPlugin", "getArch", []);
-    },
-    hello: function (input, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "HelloCPlugin", "hello", [input]);
-    },
-    calculate: function (x, y, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "HelloCPlugin", "calculate", [x, y]);
+        cordova.exec(successCallback, errorCallback, "HelloCPlugin", "getKey", []);
     }
 };
